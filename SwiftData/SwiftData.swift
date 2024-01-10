8617452cd345d2cc90836502743b4b045b4cf8e2
Elaidzha1940 <eli080293@gmail.com>
2024-01-10 21:56:08 +0300

    SwiftData

---
+++ SwiftData/SwiftData.swift
@@ -0,0 +1,23 @@
+//  /*
+//
+//  Project: SwiftData
+//  File: SwiftData.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 10.01.2024
+//
+//  */
+
+import SwiftUI
+
+struct SwiftData: View {
+    var body: some View {
+        
+        VStack {
+          
+        }
+    }
+}
+
+#Preview {
+    SwiftData()
+}
---
+++ SwiftData/SwiftDataApp.swift
@@ -0,0 +1,19 @@
+//  /*
+//
+//  Project: SwiftData
+//  File: SwiftDataApp.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 10.01.2024
+//
+//  */
+
+import SwiftUI
+
+@main
+struct SwiftDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            SwiftData()
+        }
+    }
+}
