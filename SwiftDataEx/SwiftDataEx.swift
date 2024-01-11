ae509892ab71eb784ac21ca67bd5557a96767346
Elaidzha1940 <eli080293@gmail.com>
2024-01-11 04:05:21 +0300

    Added ExpensesCell + AddExpenseSheet

--- SwiftDataEx/SwiftDataEx.swift
+++ SwiftDataEx/SwiftDataEx.swift
@@ -11,14 +11,95 @@ import SwiftUI
 import SwiftData
 
 struct SwiftDataEx: View {
+    @State private var isShowingItemSheet: Bool = false
+    var expenses: [MyExpense] = []
+    
     var body: some View {
         
-        VStack {
-          
+        NavigationStack {
+            List {
+                ForEach(expenses) { expense in
+                    ExpensesCell(expense: expense)
+                }
+            }
+            .navigationTitle("MyExpenses")
+            .navigationBarTitleDisplayMode(.large)
+            .sheet(isPresented: $isShowingItemSheet, content: {
+                AddExpenseSheet()
+            })
+            .toolbar {
+                if !expenses.isEmpty {
+                    Button("Add Expense", systemImage: "plus") {
+                        isShowingItemSheet = true
+                    }
+                }
+            }
+            .overlay {
+                if expenses.isEmpty {
+                    ContentUnavailableView(label: {
+                        Label("No Expenses", systemImage: "list.bullet.rectangle.portraits")
+                    }, description: {
+                        Text("Start adding expense to see your list.")
+                    }, actions: {
+                        Button("Add Expense") { isShowingItemSheet = true}
+                    })
+                    .offset(y: -60)
+                }
+            }
         }
     }
 }
 
 #Preview {
     SwiftDataEx()
+}
+
+struct ExpensesCell: View {
+    let expense: MyExpense
+    
+    var body: some View {
+        HStack {
+            Text(expense.date, format: .dateTime.month(.abbreviated).day())
+                .frame(width: 70, alignment: .leading)
+            Text(expense.name)
+            Spacer()
+            Text(expense.value, format: .currency(code: "BTC"))
+        }
+    }
+}
+
+struct AddExpenseSheet: View {
+    @Environment(\.dismiss) private var dismiss
+    
+    @State private var name: String = ""
+    @State private var date: Date = .now
+    @State private var value: Double = 0
+    
+    var body: some View {
+        
+        let formStyle = FormStyleConfiguration()
+
+        NavigationStack {
+            Form {
+                TextField("Expense name", text: $name)
+                DatePicker("Date", selection: $date, displayedComponents: .date)
+                TextField("Value", text: $value, format: .currency(code: "BTC"))
+                    .keyboardType(.decimalPad)
+            }
+            .navigationTitle("New Expenses")
+            .navigationBarTitleDisplayMode(.large)
+            .toolbar {
+                ToolbarItemGroup(placement: .topBarLeading) {
+                    Button("Cancel") {
+                        dismiss
+                    }
+                }
+                ToolbarItemGroup(placement: .topBarLeading) {
+                    Button("Save") {
+                        let expense = MyExpense(name: name, date: date, value: value)
+                    }
+                }
+            }
+        }
+    }
 }
--- SwiftDataEx/SwiftDataExApp.swift
+++ SwiftDataEx/SwiftDataExApp.swift
@@ -13,16 +13,17 @@ import SwiftData
 @main
 struct SwiftDataExApp: App {
     
-    let container: ModelContainer = {
-        let schema = Schema([MyExpense.self])
-        let container = try! ModelContainer(for: schema, configurations: [])
-        return container
-    }()
+//    let container: ModelContainer = {
+//        let schema = Schema([MyExpense.self])
+//        let container = try! ModelContainer(for: schema, configurations: [])
+//        return container
+//    }()
     
     var body: some Scene {
         WindowGroup {
             SwiftDataEx()
         }
+        //.modelContainer(container)
         .modelContainer(for: [MyExpense.self])
     }
 }
